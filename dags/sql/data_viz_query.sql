65dceeae5800129825bc7789012d2efa0878c98d
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-02 00:19:43 +0700

    data viz sql

---
+++ dags/sql/data_viz_query.sql
@@ -0,0 +1,99 @@
+-- 1. Exchange Rate US Dollar (TABLE)
+WITH LatestExchangeRates AS (
+  SELECT currency, MAX(time_last_update_unix) AS max_time
+  FROM exchange_rates
+  WHERE base_currency = 'USD'
+  GROUP BY currency
+)
+
+SELECT er.currency, er.rates
+FROM exchange_rates AS er
+JOIN LatestExchangeRates AS ler
+ON er.currency = ler.currency AND er.time_last_update_unix = ler.max_time
+WHERE er.base_currency = 'USD'
+ORDER BY er.rates DESC;
+
+-- 2. Exhange Rates Indonesian Rupiah (TABLE)
+WITH LatestExchangeRates AS (
+  SELECT currency, MAX(time_last_update_unix) AS max_time
+  FROM exchange_rates
+  WHERE base_currency = 'IDR'
+  GROUP BY currency
+)
+
+SELECT er.currency, er.rates
+FROM exchange_rates AS er
+JOIN LatestExchangeRates AS ler
+ON er.currency = ler.currency AND er.time_last_update_unix = ler.max_time
+WHERE er.base_currency = 'IDR'
+ORDER BY er.rates DESC;
+
+-- 3. Exchange Rates Japanese Yen (TABLE)
+WITH LatestExchangeRates AS (
+  SELECT currency, MAX(time_last_update_unix) AS max_time
+  FROM exchange_rates
+  WHERE base_currency = 'JPY'
+  GROUP BY currency
+)
+
+SELECT er.currency, er.rates
+FROM exchange_rates AS er
+JOIN LatestExchangeRates AS ler
+ON er.currency = ler.currency AND er.time_last_update_unix = ler.max_time
+WHERE er.base_currency = 'JPY'
+ORDER BY er.rates DESC;
+
+-- 4. Exchange Rates Russian Rubel (TABLE)
+WITH LatestExchangeRates AS (
+  SELECT currency, MAX(time_last_update_unix) AS max_time
+  FROM exchange_rates
+  WHERE base_currency = 'RUB'
+  GROUP BY currency
+)
+
+SELECT currency, rates
+FROM exchange_rates
+WHERE (currency, time_last_update_unix) IN (
+  SELECT currency, max_time
+  FROM LatestExchangeRates
+)
+AND base_currency = 'RUB'
+ORDER BY rates DESC;
+
+-- 5. TOP 10 Currencies with the Most Stable Rates Bar chart
+WITH RateVariation AS (
+    SELECT currency, STDDEV(CAST(rates AS NUMERIC)) AS rate_stability
+    FROM `your_project_id.your_dataset_id.exchange_rates`
+    GROUP BY currency
+)
+
+SELECT currency, rate_stability
+FROM RateVariation
+ORDER BY rate_stability DESC
+LIMIT 10;
+
+-- 6. Currencies JPY with the Lowest Rates Bar Chart
+SELECT currency, MIN(CAST(rates AS NUMERIC)) AS min_rate
+FROM `your_project_id.your_dataset_id.exchange_rates`
+WHERE base_currency = 'JPY'
+GROUP BY currency
+ORDER BY min_rate
+LIMIT 10;
+
+--7. Currencies with Consistently Increasing Rates (TABLE)
+WITH RankedRates AS (
+    SELECT currency, time_last_update_unix, rates,
+        RANK() OVER (PARTITION BY currency ORDER BY time_last_update_unix) AS rank
+    FROM `your_project_id.your_dataset_id.exchange_rates`
+)
+SELECT currency, rank
+FROM RankedRates
+WHERE rank = 1;
+
+-- 8. Top 5 Currencies with the Highest Exchange Rates IDR
+SELECT currency, MAX(rates) AS max_rate
+FROM exchange_rates 
+where base_currency ='IDR' 
+GROUP BY currency
+ORDER BY max_rate DESC
+LIMIT 5;
