122a762fbe760501867f3db34987eb6054c4206d
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-05 11:08:02 +0700

    Update data_viz_query.sql

--- sql/data_viz_query.sql
+++ sql/data_viz_query.sql
@@ -1,13 +1,13 @@
 -- 1. Exchange Rate US Dollar (TABLE)
 WITH LatestExchangeRates AS (
   SELECT currency, MAX(time_last_update_unix) AS max_time
-  FROM exchange_rates
+  FROM `finpro3.exchange_rate.exchange_rate`
   WHERE base_currency = 'USD'
   GROUP BY currency
 )
 
 SELECT er.currency, er.rates
-FROM exchange_rates AS er
+FROM `finpro3.exchange_rate.exchange_rate` AS er
 JOIN LatestExchangeRates AS ler
 ON er.currency = ler.currency AND er.time_last_update_unix = ler.max_time
 WHERE er.base_currency = 'USD'
@@ -22,8 +22,8 @@ WITH LatestExchangeRates AS (
 )
 
 SELECT er.currency, er.rates
-FROM exchange_rates AS er
-JOIN LatestExchangeRates AS ler
+FROM `finpro3.exchange_rate.exchange_rate` AS er
+JOIN LatestExchangeRates AS lerq
 ON er.currency = ler.currency AND er.time_last_update_unix = ler.max_time
 WHERE er.base_currency = 'IDR'
 ORDER BY er.rates DESC;
@@ -31,13 +31,13 @@ ORDER BY er.rates DESC;
 -- 3. Exchange Rates Japanese Yen (TABLE)
 WITH LatestExchangeRates AS (
   SELECT currency, MAX(time_last_update_unix) AS max_time
-  FROM exchange_rates
+  FROM `finpro3.exchange_rate.exchange_rate`
   WHERE base_currency = 'JPY'
   GROUP BY currency
 )
 
 SELECT er.currency, er.rates
-FROM exchange_rates AS er
+FROM `finpro3.exchange_rate.exchange_rate` AS er
 JOIN LatestExchangeRates AS ler
 ON er.currency = ler.currency AND er.time_last_update_unix = ler.max_time
 WHERE er.base_currency = 'JPY'
@@ -46,13 +46,13 @@ ORDER BY er.rates DESC;
 -- 4. Exchange Rates Russian Rubel (TABLE)
 WITH LatestExchangeRates AS (
   SELECT currency, MAX(time_last_update_unix) AS max_time
-  FROM exchange_rates
+  FROM `finpro3.exchange_rate.exchange_rate`
   WHERE base_currency = 'RUB'
   GROUP BY currency
 )
 
 SELECT currency, rates
-FROM exchange_rates
+FROM `finpro3.exchange_rate.exchange_rate`
 WHERE (currency, time_last_update_unix) IN (
   SELECT currency, max_time
   FROM LatestExchangeRates
@@ -63,7 +63,7 @@ ORDER BY rates DESC;
 -- 5. TOP 10 Currencies with the Most Stable Rates Bar chart
 WITH RateVariation AS (
     SELECT currency, STDDEV(CAST(rates AS NUMERIC)) AS rate_stability
-    FROM `your_project_id.your_dataset_id.exchange_rates`
+    FROM `finpro3.exchange_rate.exchange_rate`
     GROUP BY currency
 )
 
@@ -74,7 +74,7 @@ LIMIT 10;
 
 -- 6. Currencies JPY with the Lowest Rates Bar Chart
 SELECT currency, MIN(CAST(rates AS NUMERIC)) AS min_rate
-FROM `your_project_id.your_dataset_id.exchange_rates`
+FROM `finpro3.exchange_rate.exchange_rate`
 WHERE base_currency = 'JPY'
 GROUP BY currency
 ORDER BY min_rate
@@ -84,7 +84,7 @@ LIMIT 10;
 WITH RankedRates AS (
     SELECT currency, time_last_update_unix, rates,
         RANK() OVER (PARTITION BY currency ORDER BY time_last_update_unix) AS rank
-    FROM `your_project_id.your_dataset_id.exchange_rates`
+    FROM `finpro3.exchange_rate.exchange_rate`
 )
 SELECT currency, rank
 FROM RankedRates
